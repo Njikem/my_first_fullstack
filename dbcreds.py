d6b96d441d47bf524db2080156e3dbadf3001464
Njikem <123434988+Njikem@users.noreply.github.com>
2023-06-21 18:11:52 -0600

    backup

--- dbcreds.py
+++
@@ -1,9 +0,0 @@
-conn_params = {
-    'user': 'root',
-    'password': 'Ajong',
-    'host': 'localhost',
-    'port': 3306,
-    'database': 'my_first_fullstack',
-}
-
-production_mode = False
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\njike\InnoTech\Scratch\my_first_fullstack\venv
+set VIRTUAL_ENV=C:\Users\njike\InnoTech\Scratch\my_first_full_stack\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
